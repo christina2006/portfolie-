557f7d134a00007912f159ba6f90fdff4f944fa3
Christina <38066295+christina2006@users.noreply.github.com>
2019-10-11 14:27:49 +0200

    Add files via upload

---
+++ oceanwp-child/functions.php
@@ -0,0 +1,20 @@
+<?php
+if ( !defined( 'ABSPATH' ) ) exit;
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+         
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'font-awesome','simple-line-icons','magnific-popup','slick','oceanwp-style' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+// END ENQUEUE PARENT ACTION
+
+
